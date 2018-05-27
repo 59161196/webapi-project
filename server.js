7575141e9d8900d4249b634a08b0c0378a924d1f
supatsorn <59161196@go.buu.ac.th>
2018-05-28 04:46:58 +0700

    commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+let contacts = [{ name: 'John', phoneNumber: '011-1111111' }]
+
+app.use(bodyParser.json())
+app.use(cors())
+
+/// TODO: Develop GET /contacts
+app.get('/contacts',(req,res) => {
+    res.json(contacts)
+})
+
+
+/// TODO: Develop POST /contacts
+app.post('/contacts',(req,res) => {
+    let newContact = req.body
+    contacts.push(newContact)
+    res.status(201).json(newContact)
+})
+
+
+app.listen(3000, () => {
+    console.log('API Server started at port 3000')
+})
